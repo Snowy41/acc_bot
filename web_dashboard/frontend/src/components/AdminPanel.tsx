32010d776ff25ce9996a213272fa850ee4ee7f97
Snowy41 <cooler.boyyy1@gmail.com>
2025-07-20 06:35:30 +0200

    + Updated Tags Dropdown (Admin)

--- web_dashboard/frontend/src/components/AdminPanel.tsx
+++ web_dashboard/frontend/src/components/AdminPanel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import { socket } from "../socket";
 import AdminStatsPanel from "./AdminStatsPanel";
 import Username from "./Username";
