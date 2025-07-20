0c28afd940fddf200fae29b70ad493b4809c3fa7
Snowy41 <cooler.boyyy1@gmail.com>
2025-07-21 00:21:11 +0200

    + Updated Shop-Backend

--- scripts/importShopDataToDB.js
+++ scripts/importShopDataToDB.js
@@ -1,4 +1,4 @@
-const { shopData } = require("../frontend/components/shop/shopData");
+const { shopData } = require("../frontend/components/shop/shopData.ts");
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
