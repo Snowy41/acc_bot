1e7d883af0b64a624acae87fa7aa04346177c3f3
Snowy41 <cooler.boyyy1@gmail.com>
2025-07-23 03:12:12 +0200

    + FrontEnd WhiteScreen fix?

--- web_dashboard/frontend/src/main.tsx
+++ web_dashboard/frontend/src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+console.log("React version:", React.version);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
